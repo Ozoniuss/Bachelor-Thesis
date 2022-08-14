789ddd5eafa2d0f691344520833f1184f06918dc
Ozoniuss <alexbledea_2000@yahoo.com>
2022-08-14 17:51:27 +0300

    feat(app): register new extensions and blueprints

--- app/app.py
+++ app/app.py
@@ -8,14 +8,18 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
 
-    from .extensions import db
+    from .extensions import db, bcrypt, jwt
 
     db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     from .users import bp as user_bp
     from .models import bp as model_bp
+    from .auth import bp as auth_bp
 
     app.register_blueprint(user_bp)
     app.register_blueprint(model_bp)
+    app.register_blueprint(auth_bp)
 
     return app
