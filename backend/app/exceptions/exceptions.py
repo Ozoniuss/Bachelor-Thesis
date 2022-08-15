30d13e497793dcbc5d21aeac544dbab0e11056da
Ozoniuss <alexbledea_2000@yahoo.com>
2022-08-15 21:47:44 +0300

    feat(exceptions): add custom exception classes

---
+++ backend/app/exceptions/exceptions.py
@@ -0,0 +1,16 @@
+class RequestException(Exception):
+    """
+    RequestException contains the structure of the errors returned in a request.
+    """
+
+    def __init__(self, title, details):
+        self.title = title
+        self.details = details
+
+    def as_dict(self):
+        return {"Title": self.title, "Details": self.details}
+
+
+class TrainingParametersException(RequestException):
+    def __init__(self, details):
+        super().__init__("Invalid training parameters", details)
