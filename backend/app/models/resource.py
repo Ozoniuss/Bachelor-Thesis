0f476eddd01ac19638fb9206c0ce3427947da17e
Ozoniuss <alexbledea_2000@yahoo.com>
2022-08-19 13:26:25 +0300

    refactor(models): move public interface to public

--- backend/app/models/resource.py
+++ backend/app/models/resource.py
@@ -1,47 +1,11 @@
-"""
-Contains the resource model
-"""
-
-from dataclasses import dataclass
-from typing import Union
-import uuid
+from ..public.api import (
+    ModelAttributes,
+    ModelData,
+    ModelMeta,
+    ModelRelationships,
+    Links,
+)
 from .model import Model
-
-
-@dataclass
-class Links:
-    self: Union[str, None] = None
-    related: Union[str, None] = None
-
-
-@dataclass
-class ModelAttributes:
-    name: str
-    description: str
-    created_at: str
-    updated_at: str
-    public: bool
-
-
-@dataclass
-class ModelMeta:
-    current_prediction_labels: list[str]
-
-
-@dataclass
-class ModelRelationships:
-    uploader: Links
-    last_trained_on: Union[Links, None] = None
-
-
-@dataclass
-class ModelData:
-    id: uuid.UUID
-    attributes: ModelAttributes
-    meta: ModelMeta
-    links: Links
-    relationships: ModelRelationships
-    type: str = "model"
 
 
 def from_db_entity(url: str, entity: Model):
@@ -58,7 +22,7 @@ def from_db_entity(url: str, entity: Model):
         links=Links(self=url + "/model/" + str(entity.id)),
         meta=ModelMeta(current_prediction_labels=entity.current_prediction_labels),
         relationships=ModelRelationships(
-            uploader=Links(related=url + "/users/" + str(entity.uploader)),
+            belongs_to=Links(related=url + "/users/" + str(entity.belongs_to)),
         ),
     )
 
