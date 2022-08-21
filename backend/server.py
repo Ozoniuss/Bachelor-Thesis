20c23b26e8fbc1055455444568fdd1e5514f5e1e
Ozoniuss <alexbledea_2000@yahoo.com>
2022-08-21 13:23:20 +0300

    feat(app): use socketio to run app

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,9 @@ import os
 
 from app import create_app
 
+from app.extensions import socketio
+
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=os.getenv("PORT", 5000), debug=os.getenv("DEBUG", True))
+    socketio.run(app, port=os.getenv("PORT", 5000), debug=os.getenv("DEBUG", True))
