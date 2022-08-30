ac7f4c0dcb4783828608cd0fee38f1c309ecc3e9
Ozoniuss <alexbledea_2000@yahoo.com>
2022-08-30 16:18:23 +0300

    style(): document training parameters

--- backend/app/public/websocket/training_parameter.py
+++ backend/app/public/websocket/training_parameter.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+SAMPLES_LABEL = "label"
+SAMPLES_ERROR = "error"
+
 SEED = "seed"
 BATCH_SIZE = "batch_size"
 EPOCHS = "epochs"
@@ -18,11 +21,55 @@ DEFAULT_ON_NOT_ENOUGH_SAMPLES = "error"
 DEFAULT_SAMPLE_SIZE = 1000
 DEFAULT_TRAIN_ALL_NETWORK = False
 
+MAX_SEED = 255
+MIN_SEED = 0
+
+MAX_BATCH_SIZE = 100
+MIN_BATCH_SIZE = 1
+
+MAX_EPOCHS = 30
+MIN_EPOCHS = 1
+
+MAX_LEARNING_RATE = 1
+MIN_LEARNING_RATE = 0.0001
+
+MAX_VALIDATION_SPLIT = 0.2
+MIN_VALIDATION_SPLIT = 0.2
+
+MAX_SAMPLE_SIZE = 5000
+MIN_SAMPLE_SIZE = 1
+
+ON_NOT_ENOUGH_SAMPLES_VALUES = [SAMPLES_LABEL, SAMPLES_ERROR]
+
 
 @dataclass
 class TrainingParameters:
     """
     Contains user-provided training parameters via websocket connection.
+
+    parameters:
+    - epochs: the number of times the entire dataset is processed. All training
+    samples are processed in each epoch, in batches (if applicable).
+    - batch_size: the number of samples processed before performing an
+    optimization step of the training parameters. It is the same for all epochs.
+    - learning_rate: a coefficient that multiplies the values (gradients) used
+    to adjust the training parameters which were determined during an
+    optimization step. Influences the "speed" of which the machine "learns".
+    - sample_size: the number of samples for each label.
+    - on_not_enough_samples: a  parameter that customizes the  application's
+    behaviour if there are not enough samples from a label to match the sample
+    size. Setting this to "labels" means that all available labels will be used,
+    and setting it to "error" will throw an error and stop the execution.
+    - validation_split: the ratio between the number of samples in the training
+    and validation datasets. If set to 0, a validation dataset will not be
+    generated
+    - seed: a parameter that influences which images are used for training and
+    which images are used for validation in the random selecting process. This
+    is only useful if using all the images available; the same value will
+    produce the same training and validation datasets.
+    - train_all_network: tells whether the entire network should be trained or
+    not. If set to false, only the output layer's trainable parameters will be
+    adjusted.
     """
 
     epochs: int = DEFAULT_EPOCHS
--- backend/app/services/parameters.py
+++ backend/app/services/parameters.py
@@ -1,5 +1,19 @@
-from ..utils.filesystem import LABEL, GLOBAL, ERROR
-from ..public.websocket.training_parameter import TrainingParameters
+from ..public.websocket.training_parameter import (
+    TrainingParameters,
+    MAX_BATCH_SIZE,
+    MAX_EPOCHS,
+    MAX_LEARNING_RATE,
+    MAX_SAMPLE_SIZE,
+    MAX_SEED,
+    MAX_VALIDATION_SPLIT,
+    MIN_BATCH_SIZE,
+    MIN_SAMPLE_SIZE,
+    MIN_EPOCHS,
+    MIN_LEARNING_RATE,
+    MIN_SEED,
+    MIN_VALIDATION_SPLIT,
+    ON_NOT_ENOUGH_SAMPLES_VALUES,
+)
 
 
 class TrainingParametersException(Exception):
@@ -12,25 +26,30 @@ def validate_training_parameters(params: TrainingParameters):
     function.
     """
     errors = ""
-    if (params.seed > 256) or (params.seed < 0):
-        errors += f"Invalid seed ({params.seed}): must be between 0 and 256.\n"
+    if (params.seed > MAX_SEED) or (params.seed < MIN_SEED):
+        errors += f"Invalid seed ({params.seed}): must be between {MIN_SEED} and {MAX_SEED}.\n"
 
-    if (params.batch_size < 5) or (params.batch_size > 100):
-        errors += (
-            f"Invalid batch size ({params.batch_size}): must be between 5 and 100.\n"
-        )
+    if (params.batch_size < MIN_BATCH_SIZE) or (params.batch_size > MAX_BATCH_SIZE):
+        errors += f"Invalid batch size ({params.batch_size}): must be between {MIN_BATCH_SIZE} and {MAX_BATCH_SIZE}.\n"
 
-    if params.epochs > 30:
-        errors += f"Invalid number of epochs ({params.epochs}): must be less or equal to 30.\n"
+    if (params.epochs < MIN_EPOCHS) or (params.epochs > MAX_EPOCHS):
+        errors += f"Invalid number of epochs ({params.epochs}): must be between {MIN_EPOCHS} and {MAX_EPOCHS}.\n"
 
-    if (params.learning_rate > 0.1) or (params.learning_rate < 0.0001):
-        errors += f"Invalid learning rate ({params.learning_rate}): must be between 0.0001 and 0.1.\n"
+    if (params.sample_size < MIN_SAMPLE_SIZE) or (params.sample_size > MAX_SAMPLE_SIZE):
+        errors += f"Invalid sample size ({params.sample_size}): must be between {MIN_SAMPLE_SIZE} and {MAX_SAMPLE_SIZE}.\n"
 
-    if (params.validation_split < 0) or (params.validation_split > 0.5):
-        errors += f"Invalid validation split ({params.validation_split}): must be between 0 (for no validation) and 0.5.\n"
+    if (params.learning_rate > MAX_LEARNING_RATE) or (
+        params.learning_rate < MIN_LEARNING_RATE
+    ):
+        errors += f"Invalid learning rate ({params.learning_rate}): must be between {MIN_LEARNING_RATE} and {MAX_LEARNING_RATE}.\n"
 
-    if params.on_not_enough_samples not in [LABEL, GLOBAL, ERROR]:
-        errors += f"Invalid behaviour if not enough samples ({params.on_not_enough_samples}): can only be LABEL, GLOBAL, ERROR.\n"
+    if (params.validation_split < MIN_VALIDATION_SPLIT) or (
+        params.validation_split > MAX_VALIDATION_SPLIT
+    ):
+        errors += f"Invalid validation split ({params.validation_split}): must be between {MIN_VALIDATION_SPLIT} (for no validation) and {MAX_VALIDATION_SPLIT}.\n"
+
+    if params.on_not_enough_samples not in ON_NOT_ENOUGH_SAMPLES_VALUES:
+        errors += f"Invalid behaviour if not enough samples ({params.on_not_enough_samples}): must be one of {','.join(ON_NOT_ENOUGH_SAMPLES_VALUES)}.\n"
 
     if errors != "":
         errors = errors.rstrip("\n")
