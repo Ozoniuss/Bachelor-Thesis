9228593d966775358b1c0ce3fa16654ac21cfdd3
Ozoniuss <alexbledea_2000@yahoo.com>
2022-08-30 13:26:24 +0300

    feat(server): use GPU memory growth

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,14 @@
 import os
+import tensorflow as tf
 
 from app import create_app
 
 from app.extensions import socketio
 
 app = create_app()
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 if __name__ == "__main__":
     socketio.run(app, port=os.getenv("PORT", 5000), debug=os.getenv("DEBUG", True))
